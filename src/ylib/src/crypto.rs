def6335f2b63e2ef6cfb49366c6b2d9945fa3b42
zenn <66673988+riyuzenn@users.noreply.github.com>
2022-08-11 18:28:12 +0200

    upt: update crypto

--- src/ylib/src/crypto.rs
+++ src/ylib/src/crypto.rs
@@ -24,7 +24,7 @@ use orion::aead::SecretKey;
 #[allow(dead_code)]
 pub fn nonce() -> Result<[u8; 24]> {
     let mut result = [0u8; 24];
-    getrandom::getrandom(&mut result);
+    getrandom::getrandom(&mut result).unwrap();
     Ok(result)
 }
 
@@ -63,7 +63,7 @@ fn get_key_from_password(password: &str, salt: &[u8]) -> Result<SecretKey> {
 /// ## Returns
 /// The ciphertext
 #[allow(dead_code)]
-pub fn encrypt(plaintext: impl AsRef<[u8]>, password: impl AsRef<str>, nonce: impl AsRef<[u8]>) -> Result<Vec<u8>> {
+pub fn encrypt(plaintext: impl AsRef<[u8]>, password: impl AsRef<str>) -> Result<Vec<u8>> {
     use orion::hazardous::{
         aead::xchacha20poly1305::{seal, Nonce, SecretKey as XSecretKey},
         mac::poly1305::POLY1305_OUTSIZE,
@@ -72,14 +72,15 @@ pub fn encrypt(plaintext: impl AsRef<[u8]>, password: impl AsRef<str>, nonce: im
     // Fetch param as refs
     let plaintext = plaintext.as_ref();
     let password = password.as_ref();
-    let nonce = nonce.as_ref();
+    let mut nonce = [0u8; 24];
+    getrandom::getrandom(&mut nonce).unwrap();
     // Get high-level API key
-    let key = get_key_from_password(password, nonce)?;
+    let key = get_key_from_password(password, &nonce)?;
     // Convert high-level API key to low-level API key
     let key = XSecretKey::from_slice(key.unprotected_as_bytes()).with_context(|| "Key is invalid")?;
 
     // Create a Nonce struct from the generated nonce
-    let nonce = Nonce::from_slice(nonce).with_context(|| "Nonce is too short")?;
+    let nonce = Nonce::from_slice(&nonce).with_context(|| "Nonce is too short")?;
 
     // Get the output length
     let output_len = match plaintext.len().checked_add(XCHACHA_NONCESIZE + POLY1305_OUTSIZE) {
