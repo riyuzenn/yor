f02f5d5c4d859607d1e7b0f5bd50de4200b53468
zenn <66673988+riyuzenn@users.noreply.github.com>
2022-08-13 15:53:38 +0200

    feat: main cli app

--- src/yor/src/main.rs
+++ src/yor/src/main.rs
@@ -17,6 +17,101 @@
  *
 */
 
+use std::fs;
+use ylib;
+use dialoguer::Confirm;
+use clap::Parser;
+use colored::Colorize;
+mod args;
+
+
 fn main() {
-    println!("Hello, world!");
+    ylib::initialize_env().unwrap();
+    let a: args::YorParser = args::YorParser::parse();
+    match a.command {
+        args::Op::Get(v) => {
+            let conf = ylib::get_config_data();
+            let db_name = conf.get::<String>("db_name").unwrap();
+            let db = ylib::load_db(&ylib::get_db_path(db_name.as_str()));
+            if v.keys {
+                println!("{:?}", db.get_all());
+                std::process::exit(1);
+            }
+            let key = v.key.unwrap_or_else(|| { println!("No key prompted"); std::process::exit(1); });
+            let data = ylib::get_item(db_name, key);
+            println!("{}", data.truecolor(138, 172, 171));
+        }
+        args::Op::Set(v) => {
+            let db = ylib::get_config_data();
+            let db_name = db.get::<String>("db_name").unwrap();
+            let mut pwd = db.get::<String>("db_key").unwrap_or(String::from(""));
+            if pwd  == "" && !v.no_password {
+                pwd = ylib::get_password();
+            }
+            ylib::upsert_item(db_name, pwd, v.key, v.value);
+        }
+        args::Op::SetDb(v) => {
+            let mut db = ylib::get_config_data();
+            let path = ylib::get_db_path(v.name.as_str());
+         
+            if !path.exists() {
+                println!("Database: {} not found, perhaps it doesn't exist at all?", v.name.truecolor(172, 138, 140));
+                std::process::exit(1);
+            }
+            db.set("db_name", &v.name).expect("Cannot set the database name");
+            println!("Successfully set the database to: {}", v.name.truecolor(172, 169, 138));
+        },
+        args::Op::Rem(v) => {
+            let db = ylib::get_config_data();
+            let db_name = db.get::<String>("db_name").unwrap();
+            ylib::rem_item(&db_name, &v.key).unwrap();
+        }
+        args::Op::Delete(v) => {
+            let path = ylib::get_db_path(v.name.as_str());
+
+            if !path.exists() {
+                println!("Database {} doesn't exist at all", v.name.truecolor(172, 138, 140));
+                std::process::exit(1);
+            }
+            
+            if Confirm::new().with_prompt(
+                format!(
+                    "Are you sure you want to delete: {}? (action can't be undone)", v.name
+                )
+            ).interact().unwrap() {
+                fs::remove_file(path).expect("Failed to remove the file");   
+                println!("Database: {} is removed.", v.name.truecolor(172, 138, 140));
+            } else {
+                println!("{}", "Ignoring the deletion request.".truecolor(172, 138, 140));
+            }
+            
+        },
+        args::Op::Create(v) => {
+            let path = ylib::get_db_path(v.name.as_str());
+            if path.exists() {
+                println!("It looks like database: {} is already created.", v.name.truecolor(172, 138, 140));
+                std::process::exit(1);
+            }
+            ylib::create_db(path.to_str().unwrap());
+        },
+        args::Op::ListDb => {
+            ylib::print_all_db();
+        }
+        args::Op::About => about()
+        
+    }
+}
+
+fn about() {
+    let ascii = concat!(
+        "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n",
+        "█░██░█▀▄▄▀█░▄▄▀█\n",
+        "█░▀▀░█░██░█░▀▀▄█\n",
+        "█▀▀▀▄██▄▄██▄█▄▄█\n",
+        "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n",
+    );
+    println!("\n{}", ascii.truecolor(172, 138, 172));
+    println!("{}{}", "Yor v".truecolor(198, 166, 121), env!("CARGO_PKG_VERSION").truecolor(138, 172, 171));
+    println!("{}", "─".repeat(16).truecolor(138, 152, 172));
+    println!("{}", "Yet another secure personal key-value storage vault\nfor folks who store sensitive information.\n".truecolor(160, 160, 160))
 }
