6cc65d7c71eab96455f370a072e031213f5deaa5
riyuzenn <riyuzenn@gmail.com>
2022-08-23 14:59:20 +0200

    upt: update lib

--- src/yor/src/lib.rs
+++ src/yor/src/lib.rs
@@ -1,19 +1,19 @@
 /*
  *
- *  Copyright (C) 2022-present riyuzenn
+ *  copyright (c) 2022-present riyuzenn
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
+ *  this program is free software: you can redistribute it and/or modify
+ *  it under the terms of the gnu general public license as published by
+ *  the free software foundation, either version 3 of the license, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  this program is distributed in the hope that it will be useful,
+ *  but without any warranty; without even the implied warranty of
+ *  merchantability or fitness for a particular purpose.  see the
+ *  gnu general public license for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  you should have received a copy of the gnu general public license
+ *  along with this program.  if not, see <https://www.gnu.org/licenses/>.
  *
 */
 
